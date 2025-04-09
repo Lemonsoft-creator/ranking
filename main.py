1b60eedffa2a2d71c7554b1b4fe45280f144e8bb
Lemonsoft-creator <lemonsoft1990@gmail.com>
2025-04-09 15:39:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def root():
     return FileResponse(os.path.join("static", "rangliste.html"))
+
+@app.get("/registrieren")
+def registrieren():
+    return FileResponse(os.path.join("static", "kunde_registrieren.html"))
+
+@app.get("/messung")
+def messung():
+    return FileResponse(os.path.join("static", "messung_eintragen.html"))
+
