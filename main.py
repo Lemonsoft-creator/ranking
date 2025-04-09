c70fe6af401e8b89ba9ddfedabc38a1d7865b1ab
Lemonsoft-creator <lemonsoft1990@gmail.com>
2025-04-09 16:08:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import os
 from datetime import datetime
 
 # --- Datenbank Setup ---
-DATABASE_URL = "sqlite:///./powerkube.db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
